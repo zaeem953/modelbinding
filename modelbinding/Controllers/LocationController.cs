938248792bc39dceb15768adbc3cbd633c25d887
Intern1pk <Intern1pk@DESKTOP-QPK-03>
2023-07-21 10:11:05 +0500

    Add project files.

---
+++ modelbinding/Controllers/LocationController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using modelbinding.Models;
+using System.Security.Cryptography;
+
+namespace modelbinding.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            CitiesInfo model = new CitiesInfo();
+           // model.CountryInfosList.Add(new CountryInfo { CId = 0, CName = "Select Country" });
+            model.CountryInfosList.Add(new CountryInfo { CId = 101, CName = "India" });
+            model.CountryInfosList.Add(new CountryInfo { CId = 102, CName = "Pakistan" });
+            model.CountryInfosList.Add(new CountryInfo { CId = 103, CName = "England" });
+            model.CountryInfosList.Add(new CountryInfo { CId = 104, CName = "China" });
+            model.CountryInfosList.Add(new CountryInfo { CId = 105, CName = "America" });
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult StateData(int cid) 
+        { 
+            List<StateInfo> states = new List<StateInfo>()
+            {
+                new StateInfo(){SId=1,CId=101,SName="UP"},
+                new StateInfo(){SId=2,CId=101,SName="East Punjab"},
+                new StateInfo(){SId=3,CId=102,SName="West Punjab"},
+                new StateInfo(){SId=4,CId=102,SName="Sindh"},
+                new StateInfo(){SId=5,CId=103,SName="England"},
+                new StateInfo(){SId=6,CId=103,SName="Scotland"},
+                new StateInfo(){SId=7,CId=104,SName="Guangdong"},
+                new StateInfo(){SId=8,CId=104,SName="Shangai"},
+                new StateInfo(){SId=9,CId=105,SName="California"},
+                new StateInfo(){SId=10,CId=105,SName="Newyork"},
+            };
+            CitiesInfo model = new CitiesInfo();
+            model.StateInfosList = states.Where(s=>s.CId==cid).ToList();
+            return Json(model);
+        }
+
+        [HttpPost]
+        public IActionResult CityData(int sid)
+        {
+            List<CityInfo> citi = new List<CityInfo>()
+            {
+                new CityInfo(){CityId=1,SId=1,CityName="Patna"},
+                new CityInfo(){CityId=2,SId=1,CityName="Allahbad"},
+                new CityInfo(){CityId=3,SId=2,CityName="Amritsar"},
+                new CityInfo(){CityId=4,SId=2,CityName="Chandigarh"},
+                new CityInfo(){CityId=5,SId=3,CityName="Lahore"},
+                new CityInfo(){CityId=6,SId=3,CityName="Gujranwala"},
+                new CityInfo(){CityId=7,SId=4,CityName="Karachi"},
+                new CityInfo(){CityId=8,SId=4,CityName="Hyderabad"},
+                new CityInfo(){CityId=9,SId=5,CityName="London"},
+                new CityInfo(){CityId=10,SId=5,CityName="Manchster"},
+            };
+            CitiesInfo model = new CitiesInfo();
+            model.CityInfosList = citi.Where(c => c.SId == sid).ToList();
+            return Json(model);
+        }
+    }
+}
---
+++ modelbinding/Models/CitiesInfo.cs
@@ -0,0 +1,49 @@
+﻿namespace modelbinding.Models
+{
+    public class CitiesInfo
+    {
+        private List<CountryInfo> countryInfos= new List<CountryInfo>();    
+        public List<CountryInfo> CountryInfosList
+        { 
+            get { return countryInfos; }
+            set { countryInfos = value; }
+        }
+
+        private List<StateInfo> stateInfos = new List<StateInfo>();
+        public List<StateInfo> StateInfosList
+        {
+            get { return stateInfos; }
+            set { stateInfos = value; }
+        }
+
+        //private List<CityInfo> cityInfos = new List<CityInfo>();
+        //public List<CityInfo> CityInfosList
+        //{
+        //    get { return cityInfos; }
+        //    set { cityInfos = value; }
+        //}
+
+        public List<CityInfo> CityInfosList { get; set; }
+    }
+
+    public class CountryInfo
+    {
+        public int CId { get; set; }
+        public string CName { get; set; }
+    }
+
+    public class StateInfo
+    {
+        public int SId { get; set; }
+        public int CId { get; set; }
+        public string SName { get; set; }
+    }
+
+    public class CityInfo
+    {
+        public int CityId { get; set; }
+        public int SId { get; set; }
+        public string CityName { get; set; }
+    }
+}
+    
